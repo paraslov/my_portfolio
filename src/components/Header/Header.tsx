9298b6b04ead41c8e70a4873219996bdc13f67f5
paraslov <paraslovjm@gmail.com>
2021-05-14 20:43:58 +0300

    Added Header and Navbar component structure.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navbar} from '../Navbar/Navbar';
+import s from './Header.module.css'
+
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <Navbar />
+        </div>
+    )
+}
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Navbar.module.css'
+
+
+export const Navbar = () => {
+    return (
+        <div className={s.navbar}>
+            <a href="#">Main</a>
+            <a href="#">Skills</a>
+            <a href="#">Projects</a>
+            <a href="#">Contacts</a>
+        </div>
+    )
+}
