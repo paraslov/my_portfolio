588eedb9673646a11fb1e1f79c3673507572ff27
paraslov <paraslovjm@gmail.com>
2021-07-30 06:12:40 +0300

    Folio v3.01. Navbar refactored. Main layout update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './App.scss'
-import {Header} from './components/c1-Header/Header'
 import {Main} from './components/c2-Main/Main'
 import {Skills} from './components/c3-Skills/Skills'
 import {Projects} from './components/c4-Projects/Projects';
 import {Remote} from './components/c5-Remote/Remote';
 import {ContactMe} from './components/c6-ContactMe/ContactMe';
 import {Footer} from './components/c7-Footer/Footer';
+import {Navbar} from './components/c1-Navbar/Navbar'
 
 const App = () => {
     return (
         <div className="App">
-            <Header/>
+            <Navbar/>
             <Main/>
             <Skills/>
             <Projects/>
--- src/components/c2-Main/Main.tsx
+++ src/components/c2-Main/Main.tsx
@@ -1,19 +1,58 @@
 import React from 'react'
 import commonStyles from '../../common/styles/style.module.scss'
 import s from './Main.module.scss'
+import Particles from 'react-tsparticles'
 
-
+const particlesParams = {
+    particles: {
+        color: {
+            value: 'grey',
+        },
+        links: {
+            color: '#999',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            enable: true,
+            random: false,
+            speed: 2,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                value_area: 800,
+            },
+            value: 120,
+        },
+        opacity: {
+            value: 0.8,
+        },
+        shape: {
+            type: 'circle',
+        },
+    },
+}
 
 export const Main = () => {
     return (
         <div className={`${s.mainBlock} ${commonStyles.block}`}>
+            <Particles options={particlesParams} className={s.particles}/>
             <div className={`${s.mainContainer} ${commonStyles.container}`}>
                 <div className={s.aboutMe}>
                     <span>Hi, there</span>
                     <h1>I'm Sergey Balanov</h1>
                     <p>Frontend developer</p>
                 </div>
-                <div className={s.photo}/>
+                <div className={s.photo}>
+                    <div className={s.photoFrame}/>
+                </div>
             </div>
         </div>
     )
