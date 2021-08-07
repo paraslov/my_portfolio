b088a9111da788362ef820189777ac21e996f08e
paraslov <paraslovjm@gmail.com>
2021-08-07 07:31:09 +0300

    Folio v3.08. formik lib added and implemented in ContactMe.tsx

--- src/components/c6-ContactMe/ContactMe.tsx
+++ src/components/c6-ContactMe/ContactMe.tsx
@@ -1,17 +1,62 @@
-import React, {FormEvent} from 'react'
+import React, {useState} from 'react'
 import s from './ContactMe.module.scss'
 import commonStyles from '../../common/styles/style.module.scss'
-import {Title} from '../../common/components/Title';
+import {Title} from '../../common/components/Title'
 import {Fade} from 'react-awesome-reveal'
+import axios from 'axios'
+import {FormikErrors, useFormik} from 'formik'
 
 
 export const ContactMe = () => {
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        const formData = new FormData(event.currentTarget)
-        event.preventDefault()
-        formData.forEach(d => {console.log(d)})
+    const [btnDisable, setBtnDisable] = useState(false)
+
+    //* =========================================================================== Formik validate =================>>
+    type TFormikErrors = {
+        email?: string
+        name?: string
+        message?: string
     }
+    const validate = (values: TFormikValues) => {
+        const errors: FormikErrors<TFormikErrors> = {}
+        if (!values.email) {
+            errors.email = 'This field is required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address'
+        }
+
+        if (!values.name) {
+            errors.name = 'This field is required'
+        }
+
+        if (!values.message) {
+            errors.message = 'This field is required'
+        }
+        return errors
+    }
+//* =========================================================================== Formik =============================>>
+    type TFormikValues = {
+        email: string
+        name: string
+        message: string
+    }
+    const formik = useFormik<TFormikValues>({
+        initialValues: {
+            email: '',
+            name: '',
+            message: ''
+        },
+        validate,
+        onSubmit: values => {
+            setBtnDisable(true)
+            console.log(values)
+            const {name, email, message} = values
+
+            axios.post('https://smtp-email-folio.herokuapp.com/send-message', {name, email, message})
+                .then(() => setBtnDisable(false))
+                .catch((err) => console.log(err))
+        }
+    })
 
     return (
         <div id={'contactMe'} className={`${s.contactMeBlock} ${commonStyles.block}`}>
@@ -20,15 +65,11 @@ export const ContactMe = () => {
                     <Title title={'Contact me: '}/>
                 </Fade>
                 <Fade direction={'up'} triggerOnce>
-                    <form className={s.contactMeForm} onSubmit={(e) => {
-                        handleSubmit(e)
-                        console.log(e)
-                        console.log('HIHIH')
-                    }}>
-                        <input className={s.formItem} type="text" placeholder={'Your name'} name={'name'}/>
-                        <input className={s.formItem} type="text" placeholder={'Your email'} name={'email'}/>
-                        <textarea className={s.formItem} placeholder={'Your message'} name={'message'}/>
-                        <button className={s.btn} type={'submit'}>Contact Me</button>
+                    <form className={s.contactMeForm} onSubmit={formik.handleSubmit}>
+                        <input className={s.formItem} type="text" placeholder={'Your name'} {...formik.getFieldProps('name')}/>
+                        <input className={s.formItem} type="text" placeholder={'Your email'} {...formik.getFieldProps('email')}/>
+                        <textarea className={s.formItem} placeholder={'Your message'} {...formik.getFieldProps('message')}/>
+                        <button className={s.btn} type={'submit'} disabled={btnDisable}>Contact Me</button>
                     </form>
                 </Fade>
             </div>
